34212120a735d36b397e82e2f96d8cb914694d13
Stuart Rowlands <stuart.rowlands@quantcdn.io>
2023-03-02 07:51:33 +1300

    Use helm wait and timeout instead.

--- ci/run-post-deploy.sh
+++ ci/run-post-deploy.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
 # Wait for rollout to complete..
-kubectl rollout status deploy/drupal --watch=true
+# kubectl rollout status deploy/drupal --watch=true
 
-# Wait for shutdown of terminating pods..
-SELECTOR=$(kubectl get deploy/drupal -o wide --no-headers | awk '{print $NF}')
+# # Wait for shutdown of terminating pods..
+# SELECTOR=$(kubectl get deploy/drupal -o wide --no-headers | awk '{print $NF}')
 
-while :
-do
-    POD_STATES=$(kubectl get pods --selector ${SELECTOR} --no-headers | awk '{print $3}' | uniq)
-    if [[ "$POD_STATES" == "Running" ]]; then
-        break
-    fi
+# runtime="5 minute"
+# endtime=$(date -ud "$runtime" +%s)
 
-    sleep 5
-done
+# while :
+# do
+#     POD_STATES=$(kubectl get pods --selector ${SELECTOR} --no-headers | awk '{print $3}' | uniq)
+#     if [[ "$POD_STATES" == "Running" ]] || [[ $(date -u +%s) -le $endtime ]]; then
+#         break
+#     fi
+
+#     sleep 5
+# done
 
 kubectl exec deploy/drupal -- /opt/deployment-scripts/post-deploy.sh
