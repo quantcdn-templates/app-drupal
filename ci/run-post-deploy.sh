772026d334ed78efc5a651121266f21655fcf451
Stuart Rowlands <stuart.rowlands@quantcdn.io>
2023-03-01 16:59:27 +1300

    t1.

--- ci/run-post-deploy.sh
+++ ci/run-post-deploy.sh
@@ -4,11 +4,11 @@
 kubectl rollout status deploy/drupal --watch=true
 
 # Wait for shutdown of terminating pods..
-SELECTOR=$(kubectl get deploy/drupal -o wide --no-headers | awk '{print \$NF}')
+SELECTOR=$(kubectl get deploy/drupal -o wide --no-headers | awk '{print $NF}')
 
 while :
 do
-    POD_STATES=$(kubectl get pods --selector ${SELECTOR} --no-headers | awk '{print \$3}' | uniq)
+    POD_STATES=$(kubectl get pods --selector ${SELECTOR} --no-headers | awk '{print $3}' | uniq)
     if [[ "$POD_STATES" == "Running" ]]; then
         break
     fi
