ddc3ba4170c497a3f2337061ed29041f74ceefe8
Stuart Rowlands <stuart.rowlands@quantcdn.io>
2023-03-01 17:03:26 +1300

    t2.

--- ci/run-post-deploy.sh
+++ ci/run-post-deploy.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-# Wait for rollout to complete..
-kubectl rollout status deploy/drupal --watch=true
+kubectl get pods
 
-# Wait for shutdown of terminating pods..
-SELECTOR=$(kubectl get deploy/drupal -o wide --no-headers | awk '{print $NF}')
+# # Wait for rollout to complete..
+# kubectl rollout status deploy/drupal --watch=true
 
-while :
-do
-    POD_STATES=$(kubectl get pods --selector ${SELECTOR} --no-headers | awk '{print $3}' | uniq)
-    if [[ "$POD_STATES" == "Running" ]]; then
-        break
-    fi
+# # Wait for shutdown of terminating pods..
+# SELECTOR=$(kubectl get deploy/drupal -o wide --no-headers | awk '{print $NF}')
 
-    sleep 5
-done
+# while :
+# do
+#     POD_STATES=$(kubectl get pods --selector ${SELECTOR} --no-headers | awk '{print $3}' | uniq)
+#     if [[ "$POD_STATES" == "Running" ]]; then
+#         break
+#     fi
 
-kubectl exec deploy/drupal -- echo "Call your script here.."
+#     sleep 5
+# done
+
+# kubectl exec deploy/drupal -- echo "Call your script here.."
