cfb80bc287f9286ddccd94be9cd23fe6dbc1d9a6
Stuart Rowlands <stuart.rowlands@quantcdn.io>
2023-03-07 12:29:54 +1300

    fixed token secret. added cron script. warm caches post deploy.

--- .docker/deployment-scripts/post-deploy.sh
+++ .docker/deployment-scripts/post-deploy.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-
 ## This script will run after each deployment completes.
-drush status
 
+## Cache rebuild and database updates.
 drush cr
 drush updb -y
 
+## Show the output of drush status.
+drush status
+
 ## Configuration import example.
 #drush cim -y
+
+## Warm Drupal caches.
+curl -X GET -I http://localhost
