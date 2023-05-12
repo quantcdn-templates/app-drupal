3fceafa6c4da774e1e79cd714c07a2dab4617dd2
Stuart Rowlands <stuart.rowlands@quantcdn.io>
2023-05-12 12:40:38 +1200

    Initial commit.

---
+++ .docker/quant/entrypoints/02-post-deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Run post-deployment scripts.
+# if [ "$QUANT_ENVIRONMENT_TYPE" == "development" ]; then
+#     /opt/deployment-scripts/post-deploy-development.sh
+# elif [ "$QUANT_ENVIRONMENT_TYPE" == "production" ]; then
+#     /opt/deployment-scripts/post-deploy-production.sh
+# fi
