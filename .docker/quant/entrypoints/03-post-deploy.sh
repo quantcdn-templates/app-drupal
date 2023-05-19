55522320e4cadaa00a44db5308a6b45eb9608ae5
Stuart Rowlands <stuart.rowlands@quantcdn.io>
2023-05-20 11:35:04 +1200

    Latest helm release. Added fresh installation entrypoint.

--- .docker/quant/entrypoints/03-post-deploy.sh
+++ .docker/quant/entrypoints/03-post-deploy.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
 # Run post-deployment scripts.
-# if [ "$QUANT_ENVIRONMENT_TYPE" == "development" ]; then
-#     /opt/deployment-scripts/post-deploy-development.sh
-# elif [ "$QUANT_ENVIRONMENT_TYPE" == "production" ]; then
-#     /opt/deployment-scripts/post-deploy-production.sh
-# fi
+if [ "$QUANT_ENVIRONMENT_TYPE" == "development" ]; then
+    ##
+    ## Fresh installation example.
+    ##
+    STATUS=$(drush status --fields=bootstrap --format=json)
+
+    # Drupal cannot be bootstrapped.
+    # Here we perform a fresh installation, and install the required default modules.
+    # We install devel_generate to create some sample content.
+    if [ "$(jq -r '.bootstrap' 2> /dev/null <<< "$STATUS")" != "Successful" ]; then
+        drush si -y
+        drush pm-enable devel_generate -y
+        drush devel-generate:content
+    fi
+fi
