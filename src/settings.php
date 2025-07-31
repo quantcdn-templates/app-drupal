fe0e5d8ee3f91127fccc82c233d7e01e83c784b2
Kristen Pol <kristen.pol@gmail.com>
2025-07-31 12:28:31 -0700

    Make sure site works with ddev.

--- src/settings.php
+++ src/settings.php
@@ -719,6 +719,7 @@ $settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.yml';
  */
 $settings['trusted_host_patterns'] = [
   'localhost',
+  '\.ddev.site$',
   '\.apps\.quant\.cloud$',
   '\.apps\.quantgovsites\.com$',
 ];
